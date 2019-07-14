3417885ad202ef2804b9fdd5a2646cd4ace68f03
Shaliniprakash14 <32482463+Shaliniprakash14@users.noreply.github.com>
2019-07-14 17:16:34 +0530

    Update SecureVault_DbScript.sql
    Added new column

--- SecureVault_DbScript/SecureVault_DbScript.sql
+++ SecureVault_DbScript/SecureVault_DbScript.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE `userschema1` ;
 CREATE TABLE `user` (
   `idUser` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `usertype` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idUser`)
-) ;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
